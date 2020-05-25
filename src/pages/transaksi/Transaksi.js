025a1c55b8680893c673248294550bacfb57ab0b
lukmanhidayah <lukmanhidayah01@gmail.com>
2020-05-26 02:27:54 +0700

    add react-navigation and axios

---
+++ src/pages/transaksi/Transaksi.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export class Transaksi extends Component {
+    render() {
+        return (
+            <View>
+                <Text> Transaksi </Text>
+            </View>
+        )
+    }
+}
+
+export default Transaksi
